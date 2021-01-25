d4658480710fe3d0589a1c47d816bc829db59610
Lukie Kang <lukiek@gmail.com>
2021-01-25 22:43:57 +0000

    feat: install prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
+  return <div className="App">
     
-    </div>
-  );
+  </div>;
 }
 
 export default App;
