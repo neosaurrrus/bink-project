d00d9985d1d700f168fcbf6e6fe829d346997027
Lukie Kang <lukiek@gmail.com>
2021-01-25 23:49:42 +0000

    feat: add initial rendering of categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,23 +1,34 @@
 import React, { useState, useEffect } from "react";
+import { Category } from "./Category";
 
 export const Categories = () => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
   useEffect(() => {
     loadData();
-  },[]);
+  }, []);
 
   const loadData = async () => {
     const response = await fetch(
       "https://www.themealdb.com/api/json/v1/1/categories.php"
     );
     const data = await response.json();
-    setData(data)
-
+    setData(data);
   };
+
+  if (!data)
+    return (
+      <>
+        <h2 data-testid="categories-heading">Categories</h2>
+        <h3>Loading Data</h3>
+      </>
+    );
 
   return (
     <>
       <h2 data-testid="categories-heading">Categories</h2>
+      {data.categories.map((category) => (
+        <Category key={category.idCategory} category={category} />
+      ))}
     </>
   );
 };
