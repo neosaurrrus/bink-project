7cdf86ee9b706a585c60ddf6391fa60a1347a38c
Lukie Kang <lukiek@gmail.com>
2021-01-25 22:51:38 +0000

    feat: add app placeholder title with test.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
+
 import "./App.css";
+import React, { Component } from 'react';
 
 function App() {
   return (
   <div className="App">
-
+    <h1>Meal App</h1>
   </div>
   )
 }
