a6dbe8e158dd25e04b927bb08975dfadac2fb50f
Lukie Kang <lukiek@gmail.com>
2021-01-25 23:28:23 +0000

    feat: set up api fetch request for categories and add to state

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export const Categories = () => {
-  return (
-    <div>
-      <h2 data-testid='categories-heading'>Categories</h2>
-    </div>
-  )
-}
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    loadData();
+  },[]);
 
+  const loadData = async () => {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/categories.php"
+    );
+    const data = await response.json();
+    setData(data)
+
+  };
+
+  return (
+    <>
+      <h2 data-testid="categories-heading">Categories</h2>
+    </>
+  );
+};
