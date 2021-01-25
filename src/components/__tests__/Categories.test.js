92dbfcc4e99722fc1e208b03d6bf7b5a6465769d
Lukie Kang <lukiek@gmail.com>
2021-01-25 23:15:05 +0000

    refactor: remove unused test elements from tests

--- src/components/__tests__/Categories.test.js
+++ src/components/__tests__/Categories.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import {Categories} from '../Categories';
 
 test('<Categories> renders Heading', () => {
-  const {debug, getByTestId} = render(<Categories />);
+  const {getByTestId} = render(<Categories />);
   expect(getByTestId('categories-heading').tagName).toBe('H2')
   expect(getByTestId('categories-heading').textContent).toBe('Categories')
 });
--- src/components/__tests__/Category.test.js
+++ src/components/__tests__/Category.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import {Category} from '../Category';
 
 test('<Category> renders Heading', () => {
-  const {debug, getByTestId} = render(<Category />);
+  const {getByTestId} = render(<Category />);
   expect(getByTestId('category-heading').tagName).toBe('H2')
   expect(getByTestId('category-heading').textContent).toBe('Category')
 });
