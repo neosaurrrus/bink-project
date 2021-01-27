c4deaeea9cfdb75992828caf64c8ab0dfe490c4f
Lukie Kang <lukiek@gmail.com>
2021-01-27 16:41:17 +0000

    feat: create meal card UI element for each card component

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -3,14 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Meal = ({meal, categoryName}) => {
   
+  console.log(meal)
   return (
-    <div>
+    <div class="meal-card">
+        <img alt="" src={meal.strMealThumb} />
        <Link key={meal.idMeal} to={`/${categoryName}/${meal.idMeal}`}>
           <h3>{meal.strMeal}</h3>
        </Link>
-       
     </div>
-   
   )
-
 };
