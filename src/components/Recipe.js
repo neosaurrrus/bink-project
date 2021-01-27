13dedc291927a90839aee2dd87b593fbe9a38933
Lukie Kang <lukiek@gmail.com>
2021-01-27 23:20:13 +0000

    refactor: ingredient and instruction generation moved to own functions

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,49 +1,53 @@
 import React from "react";
 import fetchToState from "../utils/fetchToState";
 
-export const Recipe = () => {
-  
+const buildIngredientList = (meal) => { // Gets the ingredients and measures in the meal and builds a combined JSX list 
+  const ingredientsArr = Object.values(meal)
+    .slice(9, 28)
+    .filter((v) => v);
+  const measuresArr = Object.values(meal).slice(29);
+
+  return ingredientsArr.map((ingredient, index) => {
+    if (ingredient && measuresArr[index])
+      return (
+        <li key={index}>
+          {measuresArr[index]} - {ingredient}{" "}
+        </li>
+      );
+  });
+}
+
+const buildInstructionList = (instructions) => { // Gets the instructions and parses into a JSX list.
+  const instructionsArr = instructions.split(". ");
+  return instructionsArr.map((instruction, index) => {
+    return <li key={index}>{instruction}</li>;
+  });
+}
+
+export const Recipe = ({match}) => {
+  console.log(match)
   const [data, loading] = fetchToState(
     `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
   );
+  if (loading) return <h1>Loading Recipe</h1>;
+  const meal = data.meals[0]
+  const { strMeal, strMealThumb, strInstructions } = meal;
 
-  if (loading) return (
-    <h1>Loading Recipe</h1>
-  )
-
-  const { strMeal, strMealThumb, strInstructions } = data.meals[0]
-  console.log(data.meals[0])
-
-  const instructionsArr = strInstructions.split(". ")
-  const listedInstructions = instructionsArr.map( (instruction,index) => {
-    return (<li key={index}>{instruction}</li>)
-  });
-
-  const ingredientsArr = Object.values(data.meals[0]).slice(9,28).filter( v => v)
-  const measuresArr = Object.values(data.meals[0]).slice(29);
-
-  const measuresAndIngredients = ingredientsArr.map ( (ingredient, index) => {
-    if (ingredient && measuresArr[index]) return (<li key={index}> {measuresArr[index]} - {ingredient} </li>)
-  })
+  const ingredientList = buildIngredientList(meal)
+  const instructionList = buildInstructionList(strInstructions)
   
- 
-  //loop through meal object and make an array for both StrIngredients and meals that are not blank
-
   return (
     <div className="meal-recipe">
-        <h2>{strMeal}</h2>
-        <img src={strMealThumb} alt=""></img>
-        <div>
-          <h3>Ingredients</h3>
-          <ul>{measuresAndIngredients}</ul>
-        </div>
-        <div>
-          <h3>Instructions</h3>
-          <ol>{listedInstructions}</ol>
-        </div>
+      <h2>{strMeal}</h2>
+      <img src={strMealThumb} alt=""></img>
+      <div>
+        <h3>Ingredients</h3>
+        <ul>{ingredientList}</ul>
+      </div>
+      <div>
+        <h3>Instructions</h3>
+        <ol>{instructionList}</ol>
+      </div>
     </div>
-  )
+  );
 };
-
-
-// if (ingredient) return (<li key={index}> `${measuresArr[index]} - ${ingredient}` </li>)
