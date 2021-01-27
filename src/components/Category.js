ce83bb3046950872c1e0aa436008ae1ddc26aee8
Lukie Kang <lukiek@gmail.com>
2021-01-27 08:31:50 +0000

    style: update variable naming and whitescape. update lintcache

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,22 +3,22 @@ import { Link } from "react-router-dom";
 import fetchToState from "../utils/fetchToState";
 
 export const Category = ({ match }) => {
-  const category = match.params.id;
+  const categoryName = match.params.id;
 
   const [data, loading] = fetchToState(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
   );
 
   if (loading)
     return (
       <>
-        <h2 data-testid="category-heading">Loading {category} Meals</h2>
+        <h2 data-testid="category-heading">Loading {categoryName} Meals</h2>
       </>
     );
   else
     return (
       <>
-        <h2 data-testid="category-loaded-heading">{category} Meals</h2>
+        <h2 data-testid="category-loaded-heading">{categoryName} Meals</h2>
         {data.meals.map((meal) => (
           <Link key={meal.idMeal} to={`/${category}/${meal.idMeal}`}>
             <h3>{meal.strMeal}</h3>
