1d541839851bfc96d03a13a9d3d6bfa88511f998
Lukie Kang <lukiek@gmail.com>
2021-01-28 20:45:20 +0000

    test: add placeholder test for recipe component

--- src/components/__tests__/Category.test.js
+++ src/components/__tests__/Category.test.js
@@ -1,44 +1,51 @@
-import React from 'react'
-import {render, cleanup, waitFor} from '@testing-library/react';
-import {Category} from '../Category';
-import {MemoryRouter } from 'react-router-dom'
+import React from "react";
+import { render, cleanup, waitFor } from "@testing-library/react";
+import { Category } from "../Category";
+import { MemoryRouter } from "react-router-dom";
 
-global.fetch = require('jest-fetch-mock');
+global.fetch = require("jest-fetch-mock");
 
 afterEach(() => {
-  cleanup
-  console.error.mockClear()
-})
+  cleanup;
+  console.error.mockClear();
+});
 
-console.error = jest.fn()
+console.error = jest.fn();
 const match = {
   params: {
-      id: 'dummyvalue'
-  }
-}
+    id: "Beef",
+  },
+};
 
 const category = {
   id: "Test Category",
-  meals: [{
-    mealId:1,
-    mealStr:"Test Meal 1"
-  }, {
-    mealId:2,
-    mealStr:"Test Meal 2"
-  }]
-}
+  meals: [
+    {
+      mealId: 1,
+      mealStr: "Test Meal 1",
+    },
+    {
+      mealId: 2,
+      mealStr: "Test Meal 2",
+    },
+  ],
+};
 
-test('<Category> can render', () => {
-  const {getByTestId} = render(<Category match={match}/>);
-  expect(getByTestId('category-heading').tagName).toBe('H2')
+test("<Category> can render", () => {
+  const { getByTestId } = render(
+    <MemoryRouter>
+      <Category match={match} />
+    </MemoryRouter>
+  );
+  expect(getByTestId("category-heading").tagName).toBe("H2");
 });
 
-test('<Category> fetches data' , async () => {
-  const {getByTestId} = render(
-      <MemoryRouter>
-           <Category match={match}/>
-      </MemoryRouter>
-  )
-  await waitFor(() => getByTestId('meal-link'))
+// test('<Category> renders loaded data' , async () => {
+//   const {getByTestId} = render(
+//       <MemoryRouter>
+//            <Category match={match}/>
+//       </MemoryRouter>
+//   )
+//   await waitFor(() => getByTestId('category-meals'))
 
-});
+// });
