46b86345886962288086fc63e543e33c314d59a2
Lukie Kang <lukiek@gmail.com>
2021-01-29 11:07:50 +0000

    refactor: remove warnings by remove redundant code

--- src/utils/fetchToState.js
+++ src/utils/fetchToState.js
@@ -2,25 +2,31 @@ import { useState, useEffect} from 'react';
 
 export default function FetchToState(url){
     //hook to fetch and store the returned data
-    let isSubscribed = true
+  
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true)
 
-    async function fetchUrl(){
-        try{
-            const response = await fetch(url)
-            const data = await response.json()
-            
-              setData(data)
-              setLoading(false)
-        } catch(e) {
-            console.log(e)
-        }
-    }
+    
+
     useEffect(() => {
-            
+
+
+        async function fetchUrl(){
+            try{
+                const response = await fetch(url)
+                const data = await response.json()
+                
+                  setData(data)
+                  setLoading(false)
+            } catch(e) {
+                console.log(e)
+            }
+        }
+
+            let isSubscribed = true
             if(isSubscribed) {fetchUrl()}
-            return () => {const localSubscribed = isSubscribed = false;}
-        },[]);
+
+            return () => {isSubscribed = false;}
+        },[url]);
     return [data,loading]
 }
